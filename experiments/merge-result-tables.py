34e0a8f3c848ad49250de4ca3853e2ea1dfeece6
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2024-01-23 11:16:48 +0200

    Bold max values per row

--- experiments/merge-result-tables.py
+++ experiments/merge-result-tables.py
@@ -5,21 +5,22 @@ import pandas as pd
 def read_md_table(file_path):
     df = pd.read_csv(
         file_path,
-        sep='|',
+        sep="|",
         skiprows=[1],
-    ).dropna(
-        axis=1,
-        how='all'
-    )
+    ).dropna(axis=1, how="all")
     df.columns = df.columns.str.strip()
     df.drop(columns=["size"], inplace=True)
     return df
 
 
-# def highlight_max(s):
-#     print(s)
-#     is_max = s == s.max()
-#     return ["*" + str(v) + "*" if is_max else v for v in s]
+def highlight_max(df):
+    styled_rows = []
+    for _, row in df.iterrows():
+        is_max = list(row == row.max())
+        styled_rows.append(
+            [f"**{v:.4f}**" if is_max[i] else f"{v:.4f}" for i, v in enumerate(row)]
+        )
+    return pd.DataFrame(styled_rows)
 
 
 def main():
@@ -30,20 +31,14 @@ def main():
     dfs = []
     for fpath in sys.argv[1:]:
         column_name = fpath.split("-", maxsplit=1)[1].rsplit(".")[0].split(":")[0]
-        dfs.append(
-            read_md_table(fpath).rename(
-                columns={"mean": column_name}
-            )
-        )
+        dfs.append(read_md_table(fpath).rename(columns={"mean": column_name}))
 
     joined_df = dfs[0]
     for tmp_df in dfs[1:]:
-        joined_df = joined_df.merge(tmp_df,
-                                    on=["language", "field"],
-                                    how='outer'
-                                    )
+        joined_df = joined_df.merge(tmp_df, on=["language", "field"], how="outer")
 
-    # styled_table = joined_df.apply(highlight_max, subset=joined_df.columns[1:])
+    cols = joined_df.columns[2:]
+    joined_df[cols] = highlight_max(joined_df[cols])
     print(joined_df.to_markdown(tablefmt="github", index=False))
 
 
